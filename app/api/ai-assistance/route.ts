57d547d6b75291c873a217b8cabdec36a5556a98
afuhflynn <flyinnsafuh@gmail.com>
2025-04-02 19:59:43 +0100

    feat: Add chat session with no stream to reduce ai response time

--- app/api/ai-assistance/route.ts
+++ app/api/ai-assistance/route.ts
@@ -44,15 +44,11 @@ export async function POST(req: Request) {
     ${code}
     \`\`\`
     
-    User query: ${prompt}
-    
     Based on the above information provide the user with tips, hints, online reference on how to complete the challenge but NOT THE ACTUAL solution. Let the user learn and figure out the solution with your help.
     
     Also provide them with advice on the importance of going thought process and figuring out the answer without using any ai tool or any other code examples out there to provide them solution. They should take their time and learn.
 
-    NB: Only respond to greetings (response should be short), friendly requests, learning, coding and motivation related questions. Else simply say Sorry I can't help out with that.
-    
-    Every response should be with respect to your chat history so that you don't repeat your self. DO NOT REPEAT YOUR SELF.
+    Remember to be friendly, kind, reply to greetings and be cool.
     `;
 
     // Create initial Chatbot history
@@ -78,29 +74,23 @@ export async function POST(req: Request) {
       config: {
         temperature: 0.5,
         maxOutputTokens: 1024,
+        systemInstruction: systemPrompt,
       },
       history: [initialUserChat, ...history],
     });
 
-    const stream = await chat.sendMessageStream({
-      message: systemPrompt,
+    // Send the message
+    const response = await chat.sendMessage({
+      message: prompt,
     });
-    for await (const chunk of stream) {
-      let text: string | undefined = "";
-      if (chunk) {
-        text = chunk.text;
-      } else {
-        throw new Error(
-          "Error fetching SyntaxSpring Code Assist response. Try again later"
-        );
-      }
-      if (text) {
-        // Send the text to the user
-        return NextResponse.json({
-          response: text,
-        });
-      }
+
+    if (!response.text) {
+      throw new Error("An error occurred. Try again later.");
     }
+    // Send the complete response back to the user
+    return NextResponse.json({
+      response: response.text,
+    });
   } catch (error) {
     console.error("AI assistance error:", error);
     return NextResponse.json(
--- components/auth/auth-buttons.tsx
+++ components/auth/auth-buttons.tsx
@@ -1,10 +1,18 @@
 import { Github } from "lucide-react";
 import { Button } from "../ui/button";
 import GoogleIcon from "../GoogleIcon";
+import { FormEvent } from "react";
+import { signIn } from "@/lib/auth";
 
 export const AuthButtons = () => {
   return (
-    <form className="flex space-x-2">
+    <form
+      className="flex space-x-2"
+      action={async () => {
+        "use server";
+        signIn("github");
+      }}
+    >
       <Button variant="secondary" className="w-full">
         <Github className="mr-2 h-4 w-4" /> Github
       </Button>
